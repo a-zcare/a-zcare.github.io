4a4aae8e40fb47c1ba085bcdb0eab974c7ebe7e7
a-zcare <azarevics.a@gmail.com>
2026-09-03 22:58:15 +0300

    Load final Android phone shell refinements

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@
   css.href = 'css/landing-v2.css?v=20260903-android-10';
   document.head.appendChild(css);
 
+  const shell = document.createElement('link');
+  shell.rel = 'stylesheet';
+  shell.href = 'css/phone-shell-v2.css?v=20260903-1';
+  document.head.appendChild(shell);
+
   const core = document.createElement('script');
   core.src = 'js/core.js?v=20260902-3';
   core.async = false;
