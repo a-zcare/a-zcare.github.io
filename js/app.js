ea79970c4994d7eaa99f7af46ebec144991b2eb0
a-zcare <azarevics.a@gmail.com>
2026-09-03 22:17:24 +0300

    Refresh legacy nav removal

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 (() => {
   const css = document.createElement('link');
   css.rel = 'stylesheet';
-  css.href = 'css/landing-v2.css?v=20260903-android-1';
+  css.href = 'css/landing-v2.css?v=20260903-android-2';
   document.head.appendChild(css);
 
   const core = document.createElement('script');
