09594ca7ec6ca153c321cc919c4fcc970532b82f
a-zcare <azarevics.a@gmail.com>
2026-09-03 22:37:44 +0300

    Refresh polished dock icons

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 (() => {
   const css = document.createElement('link');
   css.rel = 'stylesheet';
-  css.href = 'css/landing-v2.css?v=20260903-android-4';
+  css.href = 'css/landing-v2.css?v=20260903-android-5';
   document.head.appendChild(css);
 
   const core = document.createElement('script');
